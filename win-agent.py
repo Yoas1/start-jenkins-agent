8ae2bd54aacb74188d1235b9b08b619414d6702f
Yoav Abadi <63541874+Yoas1@users.noreply.github.com>
2023-01-14 11:23:11 +0200

    add status check and timestmp

--- win-agent.py
+++ win-agent.py
@@ -1,17 +1,26 @@
 import os
 from os import path
+import subprocess
+import time
+import datetime
 
-def chck_status():
-    data = os.popen('curl -u<user>:<password> <curl>').read()
+
+def time_now():
+    now = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
+    return now
+
+
+def check_offline():
+    data = os.popen('curl --silent -u<user>:<password> <url>/computer/<agent_name>/api/json').read()
     if '"offline":true' in data:
-        print("ok")
+        return "offline"
     else:
-        print("no")
-    
+        return "online"
+
 def new_agent():
-    url_config = input("Enter agent url: ") #= http://192.168.1.167:8080/computer/win/jenkins-agent.jnlp
-    secret_config = input("Enter agent secret: ") #=288758c8463a3d2e9f96918dc75f45004c7b90dab86c4b5ab294c9a694437037
-    workdir_config = input("Enter agent workdir: ") #=D:\\agent
+    url_config = input("Enter agent url: ") #= <url>/computer/<agent_name>/jenkins-agent.jnlp
+    secret_config = input("Enter agent secret: ") #= <secret>
+    workdir_config = input("Enter agent workdir: ") #= <workdir>
     with open('config', 'w') as conf:
         conf.write(url_config + '\n' + secret_config + '\n' + workdir_config + '\n')
 
@@ -24,13 +33,27 @@ def start_agent():
     url = str(list[0].replace('\n', ""))
     secret = str(list[1].replace('\n', ""))
     workdir = str(list[2].replace('\n', ""))
-    os.system('cmd /c "java -jar agent.jar -jnlpUrl {} -secret {} -workDir "{}""'.format(url, secret, workdir))
+    subprocess.Popen(["start", "cmd", "/k", 'java -jar C:\\agent\\agent.jar -jnlpUrl {} -secret {} -workDir {}'.format(url, secret, workdir)], shell = True)
 
 
 if __name__ == '__main__':
-
-    if not path.exists("config"):
-        new_agent()
-        start_agent()
-    else:
-        start_agent()
+    while True:
+        if check_offline() == "offline":
+            print("Agent start in few seconds")
+            if not path.exists("config"):
+                new_agent()
+                start_agent()
+                time.sleep(10)
+                print("Connected " + time_now())
+                time.sleep(30)
+            else:
+                start_agent()
+                time.sleep(10)
+                print("Connected " + time_now())
+                time.sleep(30)
+        elif check_offline() == "online":
+            print("Connected " + time_now())
+            time.sleep(30)
+        else:
+            print("Error " + time_now())
+            break
